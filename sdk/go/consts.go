b5c1207a118fed500109874e923bd1f58b47308f
Ben Croker <ben@putyourlightson.com>
2025-01-04 07:35:16 -0600

    Fix key conversion

--- sdk/go/consts.go
+++ sdk/go/consts.go
@@ -7,8 +7,8 @@ import "time"
 const (
     DatastarKey = "datastar"
     Version                   = "1.0.0-beta.1"
-    VersionClientByteSize     = 35374
-    VersionClientByteSizeGzip = 12962
+    VersionClientByteSize     = 35373
+    VersionClientByteSizeGzip = 12961
 
     //region Default durations
 
