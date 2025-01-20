2e2ef7ddec1768dcdf669ff2bb07793133cf841e
Ben Croker <ben@putyourlightson.com>
2025-01-20 17:12:48 -0600

    Remove redundant check in `bind` plugin

--- sdk/go/consts.go
+++ sdk/go/consts.go
@@ -7,8 +7,8 @@ import "time"
 const (
     DatastarKey = "datastar"
     Version                   = "1.0.0-beta.1"
-    VersionClientByteSize     = 36140
-    VersionClientByteSizeGzip = 13197
+    VersionClientByteSize     = 36083
+    VersionClientByteSizeGzip = 13189
 
     //region Default durations
 
