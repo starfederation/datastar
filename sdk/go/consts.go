c78114b960bb28e217abdf43214256de43c8bcf5
Ben Croker <57572400+bencroker@users.noreply.github.com>
2025-01-22 16:05:31 -0600

    Ensure `onLoad` is removed (#511)

--- sdk/go/consts.go
+++ sdk/go/consts.go
@@ -7,8 +7,8 @@ import "time"
 const (
     DatastarKey = "datastar"
     Version                   = "1.0.0-beta.2"
-    VersionClientByteSize     = 36410
-    VersionClientByteSizeGzip = 13273
+    VersionClientByteSize     = 36415
+    VersionClientByteSizeGzip = 13277
 
     //region Default durations
 
