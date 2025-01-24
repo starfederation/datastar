f082567d8e0c55f34c571d422694022d7458aed5
Patrick Marchand <mail@patrickmarchand.com>
2025-01-24 18:07:10 -0500

    sdk/test error message improvements (#521)
    * sdk/test error message improvements
    
    * sdk/ts forgot to commit the better error messages
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Ben Croker <57572400+bencroker@users.noreply.github.com>

--- sdk/test/test-post.sh
+++ sdk/test/test-post.sh
@@ -3,8 +3,12 @@
 [ ! -f "$1/input.json" ] && echo "case $1 does not have input.json" && return 1
 [ ! -f "$1/output.txt" ] && echo "case $1 does not have output.txt" && return 1
 
+rm "$1/testOutput.txt"
+
 input=$(cat "$1/input.json")
 curl -sN --json  "$input"  "$2/test" -o "$1/testOutput.txt"
+
+[ ! -f "$1/testOutput.txt" ] && echo "case $1 failed: your server did not return anything" && return 1
 
 diff -q "$1/testOutput.txt" "$1/output.txt"
 
