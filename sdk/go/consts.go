e2115e090e2b33f814c9f980b20f5ead00a082cc
Ben Croker <ben@putyourlightson.com>
2025-01-28 08:45:23 -0600

    Add HTMLSelectElement

--- sdk/go/consts.go
+++ sdk/go/consts.go
@@ -7,8 +7,8 @@ import "time"
 const (
     DatastarKey = "datastar"
     Version                   = "1.0.0-beta.2"
-    VersionClientByteSize     = 36541
-    VersionClientByteSizeGzip = 13373
+    VersionClientByteSize     = 36573
+    VersionClientByteSizeGzip = 13381
 
     //region Default durations
 
