68b2ae2d56835ae7d55a3dbcf593fb664bf4e146
Ben Croker <ben@putyourlightson.com>
2025-02-03 10:37:48 -0600

    Revert "Add search" [deploy-site]
    This reverts commit 323ec11babfa4cdb584260b35bad392dfb728633.

--- site/router.go
+++ site/router.go
@@ -5,21 +5,16 @@ import (
 	"embed"
 	"errors"
 	"fmt"
-	"io/fs"
-	"log"
 	"net/http"
-	"path/filepath"
 	"time"
 
 	"github.com/a-h/templ"
 	"github.com/benbjohnson/hashfs"
-	"github.com/blevesearch/bleve/v2"
 	"github.com/delaneyj/toolbelt"
 	"github.com/delaneyj/toolbelt/embeddednats"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/gorilla/sessions"
-	"github.com/huantt/plaintext-extractor"
 	natsserver "github.com/nats-io/nats-server/v2/server"
 )
 
@@ -29,7 +24,6 @@ var staticFS embed.FS
 var (
 	staticSys    = hashfs.NewFS(staticFS)
 	highlightCSS templ.Component
-	indexPath    = "data-star.bleve"
 )
 
 func staticPath(path string) string {
@@ -102,32 +96,11 @@ func setupRoutes(ctx context.Context, router chi.Router) (err error) {
 	}
 	ns.WaitForServer()
 
-	index, err := bleve.Open(indexPath)
-	if err == bleve.ErrorIndexPathDoesNotExist {
-		log.Printf("Creating new index...")
-		mapping := bleve.NewIndexMapping()
-		index, err = bleve.New(indexPath, mapping)
-		if err != nil {
-			log.Fatal(fmt.Errorf("failed to create index: %w", err))
-		}
-
-	} else if err != nil {
-		log.Fatal("failed to open index: %w", err)
-	} else {
-		log.Printf("Opening existing index...")
-	}
-
-	if err := indexSiteContent(ctx, index); err != nil {
-		log.Fatal("failed to index site content, ", err)
-	}
-
-	log.Println("Indexed site, index can be found at: ", indexPath)
-
 	sessionSignals := sessions.NewCookieStore([]byte("datastar-session-secret"))
 	sessionSignals.MaxAge(int(24 * time.Hour / time.Second))
 
 	if err := errors.Join(
-		setupHome(router, sessionSignals, ns, index),
+		setupHome(router, sessionSignals, ns),
 		setupGuide(ctx, router),
 		setupReferences(ctx, router),
 		setupHowTos(ctx, router),
@@ -141,57 +114,4 @@ func setupRoutes(ctx context.Context, router chi.Router) (err error) {
 	}
 
 	return nil
-}
-
-type SiteIndexDoc struct {
-	Title       string
-	Description string
-	Contents    string
-}
-
-func indexSiteContent(ctx context.Context, index bleve.Index) error {
-	markdownDir := "site/static/md"
-	extractor := plaintext.NewHtmlExtractor()
-
-	return filepath.WalkDir(markdownDir, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return fmt.Errorf("error accessing path %s: %w", path, err)
-		}
-
-		if d.IsDir() && path != markdownDir {
-			relDirName, err := filepath.Rel(markdownDir, path)
-			if err != nil {
-				return fmt.Errorf("failed to compute relative path for %s: %w", path, err)
-			}
-
-			log.Printf("Indexing directory: %s", relDirName)
-
-			dataset, err := markdownRenders(ctx, relDirName)
-			if err != nil {
-				return fmt.Errorf("failed to render markdown directory %s: %w", relDirName, err)
-			}
-
-			// walks through each file in the directory and indexes it
-			for key, value := range dataset {
-				url := fmt.Sprintf("/%s/%s", relDirName, key)
-
-				output, err := extractor.PlainText(value.Contents)
-				if err != nil {
-					return fmt.Errorf("failed to extract plain text from markdown %w", err)
-				}
-
-				doc := SiteIndexDoc{
-					Title:       value.Title,
-					Description: value.Description,
-					Contents:    *output,
-				}
-
-				if err := index.Index(url, doc); err != nil {
-					return fmt.Errorf("error indexing %s: %w", url, err)
-				}
-			}
-		}
-
-		return nil
-	})
 }
