cdc84e7d604f6e1ace81ff837e3f27384d55eace
Greg H <40124066+SpiralOSS@users.noreply.github.com>
2025-02-03 18:55:05 -0500

    sdk/dotnet model binding (#574)
    * sdk/dotnet - more renaming, doco, and code shortening; caching of signals
    
    * sdk/dotnet - added model binding
    
    * sdk/dotnet-model_binding - FromSignals behaves more like other From* attributes
    
    ---------
    
    Co-authored-by: Greg Holden <SpiralOSS@users.noreply.github.com>

--- sdk/dotnet/src/DependencyInjection/DatastarServicesProvider.fs
+++ sdk/dotnet/src/DependencyInjection/DatastarServicesProvider.fs
@@ -1,28 +1,13 @@
 namespace StarFederation.Datastar.DependencyInjection
 
-open System
 open System.Runtime.CompilerServices
-open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.DependencyInjection
-
-module DatastarServicesProvider =
-
-    let create (serviceCollection:IServiceCollection) =
-        serviceCollection.AddHttpContextAccessor() |> ignore
-
-        serviceCollection
-            .AddScoped<IServerSentEventService>(fun (svcPvd:IServiceProvider) ->
-                let httpContextAccessor = svcPvd.GetService<IHttpContextAccessor>()
-                ServerSentEventService(httpContextAccessor)
-                )
-            .AddScoped<ISignals>(fun (svcPvd:IServiceProvider) ->
-                let httpContextAccessor = svcPvd.GetService<IHttpContextAccessor>()
-                ReadSignalsService(httpContextAccessor)
-                )
 
 [<Extension>]
 type ServiceCollectionExtensionMethods() =
-
     [<Extension>]
-    static member AddDatastar serviceCollection =
-        DatastarServicesProvider.create serviceCollection
+    static member AddDatastar (serviceCollection:IServiceCollection) =
+        serviceCollection
+            .AddHttpContextAccessor()
+            .AddScoped<IDatastarServerSentEventService, ServerSentEventService>()
+            .AddScoped<IDatastarSignalsReaderService, SignalsReaderService>()
