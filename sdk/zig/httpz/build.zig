50876fe5e101c8e3ec6d1c91ddc25b3140aeb4ce
Roman Frołow <rofrol@gmail.com>
2025-02-04 19:28:36 +0100

    update zig to work on master

--- sdk/zig/httpz/build.zig
+++ sdk/zig/httpz/build.zig
@@ -22,7 +22,7 @@ pub fn build(b: *std.Build) void {
             .root_source_file = b.path("src/root.zig"),
             .target = target,
             .optimize = optimize,
-            .test_runner = b.path("test_runner.zig"),
+            .test_runner = .{ .path = b.path("test_runner.zig"), .mode = .simple },
         });
 
         tests.root_module.addImport("httpz", httpz);
