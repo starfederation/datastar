64e0e1ea93a0d2376b214488555d0113e99f967f
Johnathan Stevers <jmstevers@gmail.com>
2025-02-05 10:08:37 -0600

    separate ordered data lines from unordered data lines + only remove space after colon if its after a key at the beginning of the line

--- sdk/test/normalize.sh
+++ sdk/test/normalize.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
-# remove single space after colons if any
-sed 's/: \([^ ]\|$\)/:\1/g' "$1" |
+# remove single space after colon after key at the beginning of the line
+sed 's/^\([^:]*\): /\1:/' "$1" |
   awk '
     # events are separated by '\n\n\n'
     BEGIN { RS = "\n\n\n"; FS = "\n"; }
     {
-      # sort each line in the event
-      print $0 | "sort"
+      # fragments and script
+      ordered = ""
+      # everything else
+      unordered = ""
+
+      # separate out ordered data lines
+      for (i = 1; i <= NF; i++) {
+        if ($i ~ /^data:(fragments|script)/)
+          ordered = ordered $i "\n"
+        else
+          unordered = unordered $i "\n"
+      }
+
+      print unordered | "sort"
       close("sort", "to")
+      print ordered;
     }
   '
