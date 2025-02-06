35b49932a8733a177d6a3d41f4ed29a27e717e17
Johnathan Stevers <jmstevers@gmail.com>
2025-02-06 09:59:28 -0600

    don't remove space after colon

--- sdk/test/normalize.sh
+++ sdk/test/normalize.sh
@@ -1,26 +1,24 @@
 #!/bin/sh
 
-# remove single space after colon after key at the beginning of the line
-sed 's/^\([^:]*\): /\1:/' "$1" |
-  awk '
-    # events are separated by '\n\n\n'
-    BEGIN { RS = "\n\n\n"; FS = "\n"; }
-    {
-      # fragments and script
-      ordered = ""
-      # everything else
-      unordered = ""
+awk '
+  # events are separated by '\n\n\n'
+  BEGIN { RS = "\n\n\n"; FS = "\n"; }
+  {
+    # fragments and script
+    ordered = ""
+    # everything else
+    unordered = ""
 
-      # separate out ordered data lines
-      for (i = 1; i <= NF; i++) {
-        if ($i ~ /^data:(fragments|script)/)
-          ordered = ordered $i "\n"
-        else
-          unordered = unordered $i "\n"
-      }
-
-      print unordered | "sort"
-      close("sort", "to")
-      print ordered;
+    # separate out ordered data lines
+    for (i = 1; i <= NF; i++) {
+      if ($i ~ /^data:(fragments|script)/)
+        ordered = ordered $i "\n"
+      else
+        unordered = unordered $i "\n"
     }
-  '
+
+    print unordered | "sort"
+    close("sort", "to")
+    print ordered;
+  }
+'
