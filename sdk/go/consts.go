afa40c83aa137ba736310eda37c9e2187b7bc392
Delaney <delaneygillilan@gmail.com>
2025-02-09 15:27:40 -0800

    Idiomorph v0.5.0 breakage (#638)
    Fixes #637

--- sdk/go/consts.go
+++ sdk/go/consts.go
@@ -7,8 +7,8 @@ import "time"
 const (
     DatastarKey = "datastar"
     Version                   = "1.0.0-beta.4"
-    VersionClientByteSize     = 39815
-    VersionClientByteSizeGzip = 14745
+    VersionClientByteSize     = 39808
+    VersionClientByteSizeGzip = 14753
 
     //region Default durations
 
