7b486508307181d893e6a0c165af7624f9f491fa
Delaney Gillilan <delaneygillilan@gmail.com>
2025-02-24 09:51:18 -0800

    change to djb2 hash

--- sdk/go/consts.go
+++ sdk/go/consts.go
@@ -7,8 +7,8 @@ import "time"
 const (
     DatastarKey = "datastar"
     Version                   = "1.0.0-beta.7"
-    VersionClientByteSize     = 39533
-    VersionClientByteSizeGzip = 14666
+    VersionClientByteSize     = 39523
+    VersionClientByteSizeGzip = 14661
 
     //region Default durations
 
