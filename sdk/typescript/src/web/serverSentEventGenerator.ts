starfederation/datastar

commit 2db7468cd5fc07048a5a5eb909307688842675c6

Typescript sdk improvements (#698)
* improvements to node sdk

Add abort handling to node sdk, make onError simpler and close stream by default

 - closes https://github.com/starfederation/datastar/pull/667
 - closes https://github.com/starfederation/datastar/issues/602

* Improvements to typescript sdk readme

Ben Croker wanted a mergeSignals example

* Make typescript sdk pass tests

* add missing typescript snippets and use same name as sdk folder

* update datastar version in typescript sdk test servers

sdk/typescript/src/web/serverSentEventGenerator.ts
@@ -32,22 +32,23 @@ export class ServerSentEventGenerator extends AbstractSSEGenerator {
    * Initializes the server-sent event generator and executes the streamFunc function.
    *
    * @param onStart - A function that will be passed the initialized ServerSentEventGenerator class as it's first parameter.
-   * @param options? - An object that can contain options for the Response constructor as well as onError and onCancel callbacks.
+   * @param options? - An object that can contain options for the Response constructor onError and onCancel callbacks and a keepalive boolean.
    * The onAbort callback will be called whenever the request is aborted or the stream is cancelled
-   * The onError callback keeps the stream open after an exception  to report the error. If it is not provided it will
-   * cancel the stream and throw the error.
+   * The onError callback will be called whenever an error is met. If provided, the onAbort callback will also be executed.
+   * If an onError callback is not provided, then the stream will be ended and the error will be thrown up.
+   * If responseInit is provided, then it will be passed to the Response constructor along with the default headers.
+   * When keepalive is true (default is false), the stream will be kept open indefinitely,
+   * otherwise it will be closed when the onStart callback finishes.
    *
    * @returns an HTTP Response
    */
   static stream(
     onStart: (stream: ServerSentEventGenerator) => Promise<void> | void,
     options?: Partial<{
-      onError: (
-        stream: ServerSentEventGenerator,
-        error: unknown,
-      ) => Promise<void> | void;
+      onError: (error: unknown) => Promise<void> | void;
       onAbort: (reason: string) => Promise<void> | void;
-      init: ResponseInit;
+      responseInit: ResponseInit;
+      keepalive: boolean;
     }>,
   ): Response {
     const readableStream = new ReadableStream({
@@ -55,21 +56,24 @@ export class ServerSentEventGenerator extends AbstractSSEGenerator {
         const generator = new ServerSentEventGenerator(controller);
 
         try {
-          const startedStream = onStart(generator);
-          if (startedStream instanceof Promise) await startedStream;
+          const stream = onStart(generator);
+          if (stream instanceof Promise) await stream;
+          if (options?.keepalive) {
+            controller.close();
+          }
         } catch (error) {
-          const abortResult = options && options.onAbort
-            ? options.onAbort(
-              error instanceof Error
-                ? error.message
-                : "onStart callback threw an error",
-            )
+          const errorMsg = error instanceof Error
+            ? error.message
+            : "onStart callback threw an error";
+          const abortResult = options?.onAbort
+            ? options.onAbort(errorMsg)
             : null;
 
           if (abortResult instanceof Promise) await abortResult;
           if (options && options.onError) {
-            const errorStream = options.onError(generator, error);
-            if (errorStream instanceof Promise) await errorStream;
+            const onError = options.onError(error);
+            if (onError instanceof Promise) await onError;
+            controller.close();
           } else {
             controller.close();
             throw error;
@@ -88,7 +92,7 @@ export class ServerSentEventGenerator extends AbstractSSEGenerator {
       readableStream,
       deepmerge({
         headers: sseHeaders,
-      }, options?.init ?? {}),
+      }, options?.responseInit ?? {}),
     );
   }
 

sdk/typescript/src/node/serverSentEventGenerator.ts
@@ -25,9 +25,13 @@ export class ServerSentEventGenerator extends AbstractSSEGenerator {
     this.res = res;
 
     this.res.writeHead(200, sseHeaders);
-    this.req.on("close", () => {
-      this.res.end();
-    });
+  }
+
+  /**
+   * Closes the stream
+   */
+  public close() {
+    this.res.end();
   }
 
   /**
@@ -36,30 +40,51 @@ export class ServerSentEventGenerator extends AbstractSSEGenerator {
    * @param req - The NodeJS request object.
    * @param res - The NodeJS response object.
    * @param onStart - A function that will be passed the initialized ServerSentEventGenerator class as it's first parameter.
+   * @param options? - An object that can contain onError and onCancel callbacks as well as a keepalive boolean.
+   * The onAbort callback will be called whenever the request is aborted
+   * The onError callback will be called whenever an error is met. If provided, the onAbort callback will also be executed.
+   * If an onError callback is not provided, then the stream will be ended and the error will be thrown up.
+   * When keepalive is true (default is false), the stream will be kept open indefinitely,
+   * otherwise it will be closed when the onStart callback finishes.
    */
   static async stream(
     req: IncomingMessage,
     res: ServerResponse,
     onStart: (stream: ServerSentEventGenerator) => Promise<void> | void,
     options?: Partial<{
-      onError: (
-        stream: ServerSentEventGenerator,
-        error: unknown,
-      ) => Promise<void> | void;
+      onError: (error: unknown) => Promise<void> | void;
+      onAbort: () => Promise<void> | void;
+      keepalive: boolean;
     }>,
   ): Promise<void> {
     const generator = new ServerSentEventGenerator(req, res);
+
+    req.on("close", async () => {
+      const onAbort = options?.onAbort ? options.onAbort() : null;
+      if (onAbort instanceof Promise) await onAbort;
+
+      res.end();
+    });
+
     try {
       const stream = onStart(generator);
       if (stream instanceof Promise) await stream;
+      if (!options?.keepalive) {
+        res.end();
+      }
     } catch (error: unknown) {
-      const errorStream = options && options.onError
-        ? options.onError(generator, error)
-        : null;
-      if (errorStream instanceof Promise) await errorStream;
-    }
+      const onAbort = options?.onAbort ? options.onAbort() : null;
+      if (onAbort instanceof Promise) await onAbort;
 
-    res.end();
+      if (options?.onError) {
+        const onError = options.onError(error);
+        if (onError instanceof Promise) await onError;
+        res.end();
+      } else {
+        res.end();
+        throw error;
+      }
+    }
   }
 
   protected override send(

sdk/typescript/src/abstractServerSentEventGenerator.ts
@@ -44,7 +44,7 @@ export abstract class ServerSentEventGenerator {
 
     const typeLine = [`event: ${event}\n`];
     const idLine = eventId ? [`id: ${eventId}\n`] : [];
-    const retryLine = [
+    const retryLine = !retryDuration || retryDuration === 1000 ? [] : [
       `retry: ${retryDuration ?? DefaultSseRetryDurationMs}\n`,
     ];
 

sdk/typescript/src/node/node.ts
@@ -10,7 +10,7 @@ const server = createServer(async (req, res) => {
     const headers = new Headers({ "Content-Type": "text/html" });
     res.setHeaders(headers);
     res.end(
-      `<html><head><script type="module" src="https://cdn.jsdelivr.net/gh/starfederation/datastar@v1.0.0-beta.1/bundles/datastar.js"></script></head><body><div id="toMerge" data-signals-foo="'World'" data-on-load="@get('/merge')">Hello</div></body></html>`,
+      `<html><head><script type="module" src="https://cdn.jsdelivr.net/gh/starfederation/datastar@v1.0.0-beta.7/bundles/datastar.js"></script></head><body><div id="toMerge" data-signals-foo="'World'" data-on-load="@get('/merge')">Hello</div></body></html>`,
     );
   } else if (req.url?.includes("/test")) {
     const reader = await ServerSentEventGenerator.readSignals(req);

sdk/typescript/src/web/deno.ts
@@ -7,7 +7,7 @@ serve(async (req: Request) => {
 
   if (url.pathname === "/") {
     return new Response(
-      `<html><head><script type="module" src="https://cdn.jsdelivr.net/gh/starfederation/datastar@v1.0.0-beta.1/bundles/datastar.js"></script></head><body><div id="toMerge" data-signals-foo="'World'" data-on-load="@get('/merge')">Hello</div></body></html>`,
+      `<html><head><script type="module" src="https://cdn.jsdelivr.net/gh/starfederation/datastar@v1.0.0-beta.7/bundles/datastar.js"></script></head><body><div id="toMerge" data-signals-foo="'World'" data-on-load="@get('/merge')">Hello</div></body></html>`,
       {
         headers: { "Content-Type": "text/html" },
       },

commit 434189c69c3db2514d984838ba47c73df427800a

fix bug introduced in https://github.com/starfederation/datastar/pull/698
we want the stream to be ended only if keepalive is not present

sdk/typescript/src/web/serverSentEventGenerator.ts
@@ -58,7 +58,7 @@ export class ServerSentEventGenerator extends AbstractSSEGenerator {
         try {
           const stream = onStart(generator);
           if (stream instanceof Promise) await stream;
-          if (options?.keepalive) {
+          if (!options?.keepalive) {
             controller.close();
           }
         } catch (error) {