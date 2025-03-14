2742f6b5581d306f104a5cec8941afd475756ffc
Richard Humulock <rphumulock@gmail.com>
2025-03-14 18:24:47 -0400

    Fixing Java Example

--- examples/java/hello-world/src/main/java/org/example/servlets/HelloWorldServlet.java
+++ examples/java/hello-world/src/main/java/org/example/servlets/HelloWorldServlet.java
@@ -1,40 +1,45 @@
 package org.example.servlets;
 
-import StarFederation.Datastar.ServerSentEventGenerator;
-import StarFederation.Datastar.adapters.request.AbstractRequestAdapter;
-import StarFederation.Datastar.adapters.request.HttpServletRequestAdapter;
-import StarFederation.Datastar.adapters.response.AbstractResponseAdapter;
-import StarFederation.Datastar.adapters.response.HttpServletResponseAdapter;
-import StarFederation.Datastar.events.DataStore;
-import StarFederation.Datastar.events.MergeFragments;
-import StarFederation.Datastar.events.MergeFragmentsOptions;
-import StarFederation.Datastar.events.SignalReader;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import starfederation.datastar.adapters.request.AbstractRequestAdapter;
+import starfederation.datastar.adapters.request.HttpServletRequestAdapter;
+import starfederation.datastar.adapters.response.HttpServletResponseAdapter;
+import starfederation.datastar.events.MergeFragments;
+import starfederation.datastar.utils.DataStore;
+import starfederation.datastar.utils.ServerSentEventGenerator;
+import starfederation.datastar.utils.SignalReader;
 
-import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class HelloWorldServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        try {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try (ServerSentEventGenerator sse = new ServerSentEventGenerator(new HttpServletResponseAdapter(response))) {
             AbstractRequestAdapter requestAdapter = new HttpServletRequestAdapter(request);
-            AbstractResponseAdapter responseAdapter = new HttpServletResponseAdapter(response);
-            ServerSentEventGenerator sse = new ServerSentEventGenerator(responseAdapter);
 
             DataStore store = new DataStore();
             SignalReader.readSignals(requestAdapter, store.getStore());
-            MergeFragmentsOptions options = MergeFragmentsOptions.create();
 
             String message = "Hello, World!";
             Number delayNumber = (Number) store.getStore().get("delay");
 
             for (int i = 0; i < message.length(); i++) {
                 String htmlFragment = String.format("<div id=\"message\">%s</div>", message.substring(0, i + 1));
-                MergeFragments event = new MergeFragments(htmlFragment, options);
-                sse.MergeFragments(event);
-                Thread.sleep(delayNumber.longValue());
+                MergeFragments event = MergeFragments.builder()
+                        .selector("#message")
+                        .useViewTransition(true)
+                        .data(htmlFragment)
+                        .build();
+
+                sse.send(event);
+                try {
+                    TimeUnit.MILLISECONDS.sleep(delayNumber.longValue());
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
