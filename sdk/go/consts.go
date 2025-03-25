e4de6bfc215dd48fbdee08bb20f4b081a7606333
Ben Croker <ben@putyourlightson.com>
2025-03-24 18:12:33 -0600

    Clean up

--- sdk/go/consts.go
+++ sdk/go/consts.go
@@ -7,8 +7,8 @@ import "time"
 const (
     DatastarKey = "datastar"
     Version                   = "1.0.0-beta.10"
-    VersionClientByteSize     = 39947
-    VersionClientByteSizeGzip = 14856
+    VersionClientByteSize     = 39853
+    VersionClientByteSizeGzip = 14855
 
     //region Default durations
 
