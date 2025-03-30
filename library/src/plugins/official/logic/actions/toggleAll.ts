ccc7ea427a39f368b9d7d202f5f42c018525bb85
Ben Croker <57572400+bencroker@users.noreply.github.com>
2025-03-29 18:37:40 -0600

    Add single/double asterisk wildcards (#806)
    * Add single/double asterisk wildcards
    
    * Update release note

--- library/src/plugins/official/logic/actions/toggleAll.ts
+++ library/src/plugins/official/logic/actions/toggleAll.ts
@@ -4,22 +4,14 @@
 // Description: Toggle all signals that match one or more space-separated paths in which `*` can be used as a wildcard
 
 import { type ActionPlugin, PluginType } from '../../../../engine/types'
-import { pathMatchesPattern } from '../../../../utils/paths'
-import { trimDollarSignPrefix } from '../../../../utils/text'
+import { applyToMatchingPaths } from '../../../../utils/paths'
 
 export const ToggleAll: ActionPlugin = {
   type: PluginType.Action,
   name: 'toggleAll',
   fn: ({ signals }, paths: string) => {
-    let patterns = paths.split(/\s+/).filter((p) => p !== '')
-    patterns = patterns.map((p) => trimDollarSignPrefix(p))
-
-    for (const pattern of patterns) {
-      signals.walk((path, signal) => {
-        if (pathMatchesPattern(path, pattern)) {
-          signal.value = !signal.value
-        }
-      })
-    }
+    applyToMatchingPaths(signals, paths, (signal) => {
+      signal.value = !signal.value
+    })
   },
 }
