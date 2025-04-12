686d12ad2a93fb6ddc73dee5b54598f2eb1372ce
Dmitry Kotik <7944694+dkotik@users.noreply.github.com>
2025-04-11 20:34:15 -0400

    Update comments and documentation for Golang SDK (#827)
    * externalize interface compatibility for two optional template engines
    
    Templ and GoStar are currently module dependencies for Golang SDK. They
    should not be required for 1.0 release. This step replaces two component
    interfaces with copies that ensure compatibility without having to
    include various optional engines as dependencies with each Datastar
    deployment.
    
    * update comments and documentation for Golang SDK
    
    Added comments to `execute.go` file. I will make similar changes to
    other files. I submit those as a way of getting feedback.
    
    The only public API change is the removal of execute script options
    struct. It is changed to private by letter case because the options
    pattern is used to configure script execution. The struct serves no
    purpose and pollutes the public API.

--- sdk/go/execute.go
+++ sdk/go/execute.go
@@ -7,33 +7,48 @@ import (
 	"time"
 )
 
-type ExecuteScriptOptions struct {
+// executeScriptOptions hold script options that will be translated to [SSEEventOptions].
+type executeScriptOptions struct {
 	EventID       string
 	RetryDuration time.Duration
 	Attributes    []string
 	AutoRemove    *bool
 }
 
-type ExecuteScriptOption func(*ExecuteScriptOptions)
+// ExecuteScriptOption configures script execution event that will be sent to the client.
+type ExecuteScriptOption func(*executeScriptOptions)
 
+// WithExecuteScriptEventID configures an optional event ID for the script execution event.
+// The client message field [lastEventId] will be set to this value.
+// If the next event does not have an event ID, the last used event ID will remain.
+//
+// [lastEventId]: https://developer.mozilla.org/en-US/docs/Web/API/MessageEvent/lastEventId
 func WithExecuteScriptEventID(id string) ExecuteScriptOption {
-	return func(o *ExecuteScriptOptions) {
+	return func(o *executeScriptOptions) {
 		o.EventID = id
 	}
 }
 
+// WithExecuteScriptRetryDuration overrides the [DefaultRetryDuration] for this script
+// execution only.
 func WithExecuteScriptRetryDuration(retryDuration time.Duration) ExecuteScriptOption {
-	return func(o *ExecuteScriptOptions) {
+	return func(o *executeScriptOptions) {
 		o.RetryDuration = retryDuration
 	}
 }
 
+// WithExecuteScriptAttributes overrides the default script attribute
+// value `type module`, which renders as `<script type="module">` in client's browser.
+// Each attribute should include a pair of plain words representing the attribute name and value
+// without any formatting.
 func WithExecuteScriptAttributes(attributes ...string) ExecuteScriptOption {
-	return func(o *ExecuteScriptOptions) {
+	return func(o *executeScriptOptions) {
 		o.Attributes = attributes
 	}
 }
 
+// WithExecuteScriptAttributeKVs is an alternative option for [WithExecuteScriptAttributes].
+// Even parameters are keys, odd parameters are their values.
 func WithExecuteScriptAttributeKVs(kvs ...string) ExecuteScriptOption {
 	if len(kvs)%2 != 0 {
 		panic("WithExecuteScriptAttributeKVs requires an even number of arguments")
@@ -46,14 +61,16 @@ func WithExecuteScriptAttributeKVs(kvs ...string) ExecuteScriptOption {
 	return WithExecuteScriptAttributes(attributes...)
 }
 
+// WithExecuteScriptAutoRemove requires the client to eliminate the script element after its execution.
 func WithExecuteScriptAutoRemove(autoremove bool) ExecuteScriptOption {
-	return func(o *ExecuteScriptOptions) {
+	return func(o *executeScriptOptions) {
 		o.AutoRemove = &autoremove
 	}
 }
 
+// ExecuteScript runs a script in the client browser. Seperate commands with semicolons.
 func (sse *ServerSentEventGenerator) ExecuteScript(scriptContents string, opts ...ExecuteScriptOption) error {
-	options := &ExecuteScriptOptions{
+	options := &executeScriptOptions{
 		RetryDuration: DefaultSseRetryDuration,
 		Attributes:    []string{"type module"},
 	}
