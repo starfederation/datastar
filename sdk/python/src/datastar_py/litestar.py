f5379ff756f88cbf02dc623aedc04298159290a6
Chase Sterling <chase.sterling@gmail.com>
2025-05-05 14:52:46 -0400

    Add read_signals for litestar

--- sdk/python/src/datastar_py/litestar.py
+++ sdk/python/src/datastar_py/litestar.py
@@ -1,16 +1,18 @@
 from __future__ import annotations
 
+import json
 from functools import wraps
-from typing import Any, Callable, TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, Callable
 
 from litestar.response import Stream
 
 from .sse import SSE_HEADERS, ServerSentEventGenerator
 
 if TYPE_CHECKING:
+    from litestar import Request
     from litestar.types.helper_types import StreamType
 
-__all__ = ["SSE_HEADERS", "DatastarSSE", "ServerSentEventGenerator"]
+__all__ = ["SSE_HEADERS", "DatastarSSE", "ServerSentEventGenerator", "read_signals"]
 
 
 class DatastarSSE(Stream):
@@ -21,14 +23,18 @@ class DatastarSSE(Stream):
         **kwargs: Any,
     ) -> None:
         """
-        Similar to litestar's ServerSentEvent, but since our event generator just returns text we don't need
-        anything fancy.
+        Similar to litestar's ServerSentEvent, but since our event generator just returns text we
+        don't need anything fancy.
         """
         kwargs["headers"] = {**SSE_HEADERS, **kwargs.get("headers", {})}
         kwargs["media_type"] = "text/event-stream"
         # Removing this argument allows the class to be used as a 'response_class' on a route
         kwargs.pop("type_encoders", None)
-        super().__init__(
-            content,
-            **kwargs,
-        )
+        super().__init__(content, **kwargs)
+
+
+async def read_signals(request: Request) -> dict[str, Any]:
+    if request.method == "GET":
+        return json.loads(request.query_params["datastar"])
+    else:
+        return await request.json()
