25ca3423d8db3bcf3895a7d5a3adce1841889808
mariogk <mariogk01@gmail.com>
2025-05-14 16:55:20 +0100

    Removing other examples and just keeping one for example and integration testing

--- sdk/dotnet/csharp/src/Examples/Starfederation.Datastar.MVC/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Starfederation.Datastar.MVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
