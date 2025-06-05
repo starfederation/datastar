d6bd945eb6faf1ef8ef757a139f1ef9145f96397
Chase Sterling <chase.sterling@gmail.com>
2025-06-05 10:53:07 -0400

    Switch quart to using DatastarResponse like the other frameworks

--- sdk/python/src/datastar_py/quart.py
+++ sdk/python/src/datastar_py/quart.py
@@ -13,30 +13,28 @@ if TYPE_CHECKING:
 
 __all__ = [
     "SSE_HEADERS",
+    "DatastarResponse",
     "ServerSentEventGenerator",
-    "make_datastar_response",
     "read_signals",
 ]
 
 
-async def make_datastar_response(
-    content: DatastarEvents = None,
-    status_or_headers: int | Mapping[str, str] | None = None,
-    headers: Mapping[str, str] | None = None,
-    /,
-) -> Response:
+class DatastarResponse(Response):
     """Respond with 0..N `DatastarEvent`s"""
-    if status_or_headers is not None and not isinstance(status_or_headers, int):
-        status, headers = None, status_or_headers
-    else:
-        status = status_or_headers
-    if not content:
-        return Response(status=status or 204, headers=headers)
-    headers = {**SSE_HEADERS, **(headers or {})}
-    response = Response(content, status=status, headers=headers)
-    if isgenerator(content) or isasyncgen(content):
-        response.timeout = None
-    return response
+
+    def __init__(
+        self,
+        content: DatastarEvents = None,
+        status: int | None = None,
+        headers: Mapping[str, str] | None = None,
+    ) -> None:
+        if not content:
+            status = status or 204
+        else:
+            headers = {**SSE_HEADERS, **(headers or {})}
+        super().__init__(content, status=status, headers=headers)
+        if isgenerator(content) or isasyncgen(content):
+            self.timeout = None
 
 
 async def read_signals() -> dict[str, Any] | None:
