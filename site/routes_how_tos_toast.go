68f233d53aa74e7ae4e518932bf1f758c2658763
gamachexx <31936427+olgam4@users.noreply.github.com>
2025-06-14 18:00:00 -0400

    Create routes_how_tos_toast.go

---
+++ site/routes_how_tos_toast.go
@@ -0,0 +1,26 @@
+package site
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/starfederation/datastar/sdk/go/datastar"
+)
+
+func setupHowTosToast(howTosToast chi.Router) error {
+
+	howTosToast.Route("/toast/data", func(dataRouter chi.Router) {
+
+		dataRouter.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			sse := datastar.NewSSE(w, r)
+			sse.MergeFragments(`
+				<span id="indicator">Redirecting in 3 seconds...</span>
+			`)
+			time.Sleep(3 * time.Second)
+			sse.Redirect("/guide")
+		})
+	})
+
+	return nil
+}
